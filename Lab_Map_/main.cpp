999239ccc5a625518ca33e785e04fe88aa731cc4
MylesMc32 <Myles@myless-mbp>
2017-03-24 22:27:24 -0700

    Initial Commit

---
+++ Lab_Map_/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab_Map_
+//
+//  Created by Myles McKendell on 3/24/17.
+//  Copyright © 2017 Myles McKendell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
